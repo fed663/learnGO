27f9554db315c94103194e1fc34bd909f6afdee8
fed663 <fedya-ismailov-2002@mail.ru>
2026-03-01 10:10:15 +0300

    update main and recMax

--- sortSlice/main.go
+++ sortSlice/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"os"
 )
@@ -8,27 +9,27 @@ import (
 func main() {
 	testSlice := []int{228, 23, 664, 7, 0, 52, 8, 8, 5, 1337}
 	sortedSlice := sortSlice(testSlice)
-	_, err := fmt.Fprintln(os.Stdout, sortedSlice)
-	if err != nil {
-		panic(err)
-	}
+	_, _ = fmt.Fprintln(os.Stdout, sortedSlice)
 
 	testSlice2 := []int{2, 4, 6, 8}
 	sumTest := sum(testSlice2)
-	_, err = fmt.Fprintln(os.Stdout, sumTest)
-	if err != nil {
-		panic(err)
-	}
+	_, _ = fmt.Fprintln(os.Stdout, sumTest)
 
 	countTest := count(testSlice2)
-	_, err = fmt.Fprintln(os.Stdout, countTest)
+	_, _ = fmt.Fprintln(os.Stdout, countTest)
+
+	maxTest, err := max([]int{})
 	if err != nil {
-		panic(err)
+		if errors.Is(err, ErrEmptySlice) {
+			fmt.Println("expected error:", err)
+		} else {
+			fmt.Println("unexpected error:", err)
+			return
+		}
+	} else {
+		_, _ = fmt.Fprintln(os.Stdout, maxTest)
 	}
 
-	maxTest := max(testSlice2)
-	_, err = fmt.Fprintln(os.Stdout, maxTest)
-	if err != nil {
-		panic(err)
-	}
+	fastSortTest := fastSortSlice(testSlice)
+	_, _ = fmt.Fprintln(os.Stdout, fastSortTest)
 }
--- sortSlice/recMax.go
+++ sortSlice/recMax.go
@@ -1,17 +1,26 @@
 package main
 
-func max(arr []int) int {
-	if len(arr) == 2 {
-		if arr[0] > arr[1] {
-			return arr[0]
-		} else {
-			return arr[1]
-		}
+import (
+	"errors"
+)
+
+var ErrEmptySlice = errors.New("срез пустой")
+
+func max(arr []int) (int, error) {
+
+	if len(arr) == 0 {
+		return 0, ErrEmptySlice
 	}
-	elMax := max(arr[1:])
+	if len(arr) == 1 {
+		return arr[0], nil
+	}
+
+	elMax, err := max(arr[1:])
+	if err != nil {
+		return 0, err
+	}
 	if arr[0] > elMax {
-		return arr[0]
-	} else {
-		return elMax
+		return arr[0], nil
 	}
+	return elMax, nil
 }
